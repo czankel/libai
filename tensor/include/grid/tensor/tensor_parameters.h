f60a36d1132e2de52a9f1916bb5568c3b338c761
Chris Zankel <chris@zankel.net>
2025-07-08 21:40:00 -0700

    tensor: remove unused FoldOld function
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- tensor/include/grid/tensor/tensor_parameters.h
+++ tensor/include/grid/tensor/tensor_parameters.h
@@ -461,55 +461,6 @@ void FoldBroadcast(TOp&& op, std::span<const size_t, R> dims, auto... strides)
   }
 }
 
-
-/// @brief Helper function to reduce the rank in case of contiguous data for binary operators.
-///
-/// The operator function is called with a boolean flag to indicate that the "lowest" dimension
-/// is contiguous (strides are all implicit 1), and a new dimension span for the remaining
-/// dimension or non-contiguous dimensions.
-template <size_t TRank, typename TOp>
-void FoldOld(TOp&& op, std::span<const size_t, TRank> dimensions, auto... strides)
-{
-  if constexpr (TRank < 1)
-    op(std::span<const size_t, 0>(), false);
-
-  else if constexpr (((strides.size() == 0) || ...))
-    op(std::move(dimensions), false);
-
-  else if constexpr (TRank == 1 || ((strides.size() == 1) || ...))
-    op(std::move(dimensions), ((strides[0] == 1) && ...));
-
-  // TRank > 1 from here on
-  else if (((strides [TRank - 1] != 1) || ...))
-    op(std::move(dimensions), false);
-
-  else
-  {
-    static_assert(sizeof...(strides) > 1);
-    constexpr size_t max_folds = std::min({strides.size()...});
-    size_t fold_dim = dimensions[TRank - 1];
-
-    auto foldfn = [&]<size_t I>() -> bool
-    {
-      if (I == max_folds - 1 || ((strides [strides .size() -I - 2] - fold_dim != 0) || ...))
-      {
-        std::array<size_t, TRank - I> dim;
-        std::ranges::copy(dimensions.template first<TRank - I - 1>(), dim.begin());
-        dim[TRank - I - 1] = fold_dim;
-        op(std::span<const size_t, TRank - I>(dim.begin(), TRank - I), true);
-        return false;
-      }
-      fold_dim *= dimensions[TRank - I - 2];
-      return true;
-    };
-
-    [&] <std::size_t... I>(std::index_sequence<I...>)
-    {
-      (foldfn.template operator()<I>() && ...);
-    }(std::make_index_sequence<max_folds>{});
-  }
-}
-
 #endif // !defined(__CUDACC__)
 
 } // end of namespace grid
