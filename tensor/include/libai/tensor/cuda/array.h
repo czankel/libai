542b060905c6e55e97e94079c8d790fa910f3035
Chris Zankel <chris@zankel.net>
2026-01-26 19:36:07 -0800

    tensor: move device allocators to their own files
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- tensor/include/libai/tensor/cuda/array.h
+++ tensor/include/libai/tensor/cuda/array.h
@@ -16,107 +16,11 @@
 
 #include "device.h"
 
+#include "allocator.h"
 #include "../array.h"
 #include "../tensor_parameters.h"
 
-template<typename> class CudaPointer;
-
-template <typename T>
-struct std::iterator_traits<CudaPointer<T>>
-{
-  using value_type = T;
-  using difference_type = std::ptrdiff_t;
-  using reference = T&;
-  using pointer = T*;
-  using iterator_category = std::random_access_iterator_tag;
-};
-
-
 namespace libai {
-
-void CudaMallocManaged(void** ptr, size_t size);
-void CudaFree(void* ptr);
-
-template <typename T>
-class CudaPointer
-{
-  template <class U> friend class CudaPointer;
-  struct void_type {};
-
- public:
-  using value_type = T;
-
-  CudaPointer() {}
-  CudaPointer(std::nullptr_t) {}
-
-  template <typename U>
-  requires std::is_same_v<T, const U> CudaPointer(CudaPointer<U>& rhs) : ptr_(rhs.ptr_) {}
-
-  CudaPointer(const CudaPointer& rhs) : ptr_(rhs.ptr_) {}
-  CudaPointer(CudaPointer&& rhs) : ptr_(rhs.ptr_) { rhs.ptr_ = nullptr; }
-  CudaPointer(value_type* ptr) : ptr_(ptr) {}
-
-  CudaPointer& operator=(const CudaPointer& rhs)
-  {
-    ptr_ = rhs.ptr_;
-    return *this;
-  }
-
-  CudaPointer operator=(CudaPointer&& rhs)
-  {
-    ptr_ = rhs.ptr_;
-    rhs.ptr_ = nullptr;
-    return *this;
-  }
-
-  std::conditional<std::is_void<value_type>::value, void_type, value_type>::type&
-  operator*() const { return *ptr_; }
-  T* operator->() const { return ptr_; }
-
-  auto& operator++()    { ++ptr_; return *this; }
-  auto operator++(int)  { auto tmp = this; ptr_++; return tmp; }
-  auto& operator--()    { --ptr_; return *this; }
-  auto operator--(int)  { auto tmp = this; ptr_--; return tmp; }
-
-  auto operator+(size_t elems) const { return CudaPointer(ptr_ + elems); }
-  auto operator+(CudaPointer r) const { std::ptrdiff_t diff = ptr_ + r.ptr_; return diff; }
-  auto operator-(size_t elems) const { return CudaPointer(ptr_ - elems); }
-  auto operator-(CudaPointer r) const { std::ptrdiff_t diff = ptr_ - r.ptr_; return diff; }
-
-  explicit operator bool() const { return ptr_ != nullptr; }
-
-  friend bool operator==(CudaPointer l, std::nullptr_t)   { return l.ptr_ == nullptr; }
-  friend bool operator==(CudaPointer l, CudaPointer r)    { return l.ptr_ == r.ptr_; }
-  friend bool operator<(CudaPointer l, CudaPointer r)     { return l.ptr_ > r.ptr_; }
-  friend bool operator>(CudaPointer l, CudaPointer r)     { return l.ptr_ < r.ptr_; }
-
- private:
-  value_type* ptr_ = nullptr;
-};
-
-
-template <typename T>
-class CudaAllocator
-{
- public:
-  using value_type = T;
-  using pointer = CudaPointer<T>;
-
-  pointer allocate(size_t size)
-  {
-    void* ptr;
-    CudaMallocManaged(&ptr, size * sizeof(value_type));
-
-    return pointer(static_cast<value_type*>(ptr));
-  }
-
-  void deallocate(pointer ptr, size_t n)
-  {
-    CudaFree(&*ptr);
-  }
-};
-
-
 
 /// brief: Array is a specialization for a dynamically allocated buffer.
 template <typename T>
---
+++ tensor/include/libai/tensor/metal/allocator.h
@@ -0,0 +1,138 @@
+//
+// Copyright (C) Chris Zankel. All rights reserved.
+// This code is subject to U.S. and other copyright laws and
+// intellectual property protections.
+//
+// The contents of this file are confidential and proprietary to Chris Zankel.
+//
+
+// DO NOT INCLUDE THIS FILE DIRECTLY
+
+#ifndef LIBAI_TENSOR_METAL_ALLOCATOR_H
+#define LIBAI_TENSOR_METAL_ALLOCATOR_H
+
+namespace libai {
+template<typename> class MetalPointer;
+}
+
+template <typename T>
+struct std::iterator_traits<libai::MetalPointer<T>>
+{
+  using value_type = T;
+  using difference_type = std::ptrdiff_t;
+  using reference = T&;
+  using pointer = T*;
+  using iterator_category = std::random_access_iterator_tag;
+};
+
+
+namespace libai {
+
+template <typename T>
+class MetalPointer
+{
+  template <class U> friend class MetalPointer;
+  struct void_type {};
+
+ public:
+  using value_type = T;
+
+  MetalPointer() {}
+  MetalPointer(std::nullptr_t) {}
+
+  MetalPointer(MTL::Buffer* buffer)
+    : buffer_(buffer),
+      pointer_(reinterpret_cast<value_type*>(buffer->contents()))
+  {}
+
+  template <typename U>
+  requires std::is_same_v<T, const U>
+  MetalPointer(MetalPointer<U>& rhs) : buffer_(rhs.buffer_), pointer_(rhs.pointer_) {}
+
+  MetalPointer(const MetalPointer& rhs) : buffer_(rhs.buffer_), pointer_(rhs.pointer_) {}
+  MetalPointer(MetalPointer&& rhs) : buffer_(rhs.buffer_), pointer_(rhs.pointer_)
+  {
+    rhs.buffer_ = nullptr;
+    rhs.pointer_ = nullptr;
+  }
+
+  MetalPointer& operator=(const MetalPointer& rhs)
+  {
+    buffer_ = rhs.buffer_;
+    pointer_ = rhs.pointer_;
+    return *this;
+  }
+
+  MetalPointer operator=(MetalPointer&& rhs)
+  {
+    buffer_ = rhs.buffer_;
+    pointer_ = rhs.pointer_;
+    rhs.buffer_ = nullptr;
+    rhs.pointer_ = nullptr;
+    return *this;
+  }
+
+  std::conditional<std::is_void<value_type>::value, void_type, value_type>::type&
+  operator*() const     { return *pointer_; }
+  T* operator->() const { return pointer_; }
+
+  auto& operator++()    { ++pointer_; return *this; }
+  auto operator++(int)  { auto tmp = this; pointer_++; return tmp; }
+  auto& operator--()    { --pointer_; return *this; }
+  auto operator--(int)  { auto tmp = this; pointer_--; return tmp; }
+
+  auto operator+(size_t elems) const { return MetalPointer(pointer_ + elems); }
+  auto operator+(MetalPointer r) const { std::ptrdiff_t diff = pointer_ + r.pointer_; return diff; }
+  auto operator-(size_t elems) const { return MetalPointer(pointer_ - elems); }
+  auto operator-(MetalPointer r) const { std::ptrdiff_t diff = pointer_ - r.pointer_; return diff; }
+
+  explicit operator bool() const { return pointer_ != nullptr; }
+
+  friend bool operator==(MetalPointer l, std::nullptr_t)    { return l.pointer_ == nullptr; }
+  friend bool operator==(MetalPointer l, MetalPointer r)    { return l.pointer_ == r.pointer_; }
+  friend bool operator<(MetalPointer l, MetalPointer r)     { return l.pointer_ > r.pointer_; }
+  friend bool operator>(MetalPointer l, MetalPointer r)     { return l.pointer_ < r.pointer_; }
+
+  MTL::Buffer* Buffer()                                     { return buffer_; }
+  const MTL::Buffer* Buffer() const                         { return buffer_; }
+
+ private:
+  MTL::Buffer*  buffer_ = nullptr;
+  value_type*   pointer_ = nullptr;
+};
+
+
+template <typename T>
+class MetalAllocator
+{
+ public:
+  using value_type = T;
+  using pointer = MetalPointer<T>;
+
+  pointer allocate(size_t size)
+  {
+    size *= sizeof(value_type);
+
+    // Align up memory -- TODO only for larger sizes not < page_size?
+    if (size > vm_page_size)
+      size = vm_page_size * ((size + vm_page_size - 1) / vm_page_size);
+
+    // Allocate new buffer
+    size_t mode = MTL::ResourceStorageModeShared | MTL::ResourceHazardTrackingModeTracked;
+    auto& device = device::Metal::GetDevice();
+    auto* buffer = device.NewBuffer(size, mode);
+    if (buffer == nullptr)
+      throw std::runtime_error("failed to allocate buffer");
+    return pointer(buffer);
+  }
+
+  void deallocate(pointer p, size_t n)
+  {
+    p.Buffer()->release();
+  }
+};
+
+
+} // end of namespace libai
+
+#endif  // LIBAI_TENSOR_METAL_ALLOCATOR_H
